180fe7acd39de787075a391e14bb3888ca80019f
Glauber Carvalho <glaubercarv@gmail.com>
2020-10-02 14:52:45 -0300

    Add Eslint and Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React from "react";
-import { StatusBar } from "react-native";
-import Routes from "./routes";
-import Providers from "./providers";
+import React from 'react';
+import { StatusBar } from 'react-native';
+import Routes from './routes';
+import Providers from './providers';
+const foo = 'test';
 
 export default function App() {
   return (
